68e30401b2ec5ed6e9312c7d2ffb627db2005ff3
Dineshbabu Manoharan <thedineshbabu@hotmail.com>
2021-12-23 19:16:15 -0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let name;
+</script>
+
+<h1 class="text-3xl font-bold">
+  Hello {name} !!!
+</h1>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
